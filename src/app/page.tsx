9534c7e740f67338574c23f68e2d793e69607881
Nishant08521 <nishantkumarsingh0852@gmail.com>
2026-07-23 11:26:06 +0530

    new one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+    "use client"
 
 import { useState, useEffect } from "react"
 import { Search, LayoutDashboard, GraduationCap, Mail, PhoneCall, LogOut, Download, RefreshCw, FileText, BadgeCheck, Upload } from "lucide-react"
